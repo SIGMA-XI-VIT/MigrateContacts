7b3192f8d14b652a943ff873d0d0963143e6fc9b
AdwayB <adwaybyju@gmail.com>
2024-02-21 01:18:48 +0530

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MigrateContacts
+
+## Guidelines:
+
+- Rename the file containing the list of enrollees to `enrollees_list.xlsx` and place it in the data dir.
+- Do not change the `output_template.xlsx` file if unsure.
+- Import `contacts.csv` into Google Contacts.
+
+## Steps to use:
+
+- Ensure you have Python installed and paths to the Python exec and pip added to the Path variable.
+- Clone the repo.
+- Run `migrate-contacts.py`
+
+# Note
+
+- The script will install Pandas if it isn't present.
---
+++ migrate-contacts.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+
+os.system('pip install -r requirements.txt')
+
+output_template = pd.read_excel('data/output_template.xlsx')
+output = output_template.copy(deep=True)
+inputDf = pd.read_excel('data/enrollees_list.xlsx')
+
+
+def to_camel_case(name):
+    words = name.split()
+    if not words:
+        return ""
+    ccw = [word.capitalize() + " " for word in words]
+    return ''.join(ccw)
+
+
+inputDf["Register Number"] = inputDf.loc[inputDf["Register Number"].notna(), "Register Number"].apply(lambda x: x[:2])
+inputDf["Student Name"] = (inputDf["Student Name"].fillna("") + " " + inputDf["Register Number"].fillna(""))\
+    .apply(to_camel_case)
+inputDf = inputDf[(inputDf["Student Name"] != "") & (inputDf["Student Name"] != " ")]
+
+output["Name"] = inputDf["Student Name"]
+output["Given Name"] = inputDf["Student Name"]
+output["Group Membership"] = "* myContacts"
+output["E-mail 1 - Type"] = "Work"
+output["E-mail 1 - Value"] = inputDf["EmailId"]
+output["Phone 1 - Type"] = "Mobile"
+output["Phone 1 - Value"] = inputDf["Mobile No"]
+
+output.to_csv('data/contacts.csv', index=False)
